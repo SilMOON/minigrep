2f240419cce0075d4828bfcf4964621ec55bcff1
SilMOON <chenminghui8899@gmail.com>
2019-08-13 16:20:05 +0800

    complete simple search function

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::{fs, error::Error};
 
 pub fn run(config: Config) -> Result< (), Box<dyn Error> > {
     let contents = fs::read_to_string(config.file_name)?;
-    println!("With text:\n{}", contents);
     Ok( () )
 
 }
@@ -20,5 +19,31 @@ impl Config {
         let search_string = args[1].clone();
         let file_name = args[2].clone();
         Ok( Config { search_string, file_name })
+    }
+}
+
+fn search<'a>(search_string: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut result: Vec<&str> = vec![];
+    for line in contents.lines() {
+        if line.contains(search_string) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let search_string = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(search_string, contents));
     }
 }
